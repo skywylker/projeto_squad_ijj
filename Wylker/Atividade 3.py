61c73ed2f7f4f2c3685974cb42e445aa3a49dbbf
SkyWylker <skywylker@gmail.com>
2025-03-27 20:41:44 -0400

    Ajuste do arquivo seguindo boas práticas

--- Wylker/Atividade 3.py
+++ Wylker/Atividade 3.py
@@ -1,16 +1,22 @@
+# Programa simples para entrada de dados
+
+# Input do nome
 nome = input("Digite seu nome: ")
+
+# Input da altura
 altura = input("Digite sua altura em metros: ")
 
-print(f"Usuário: {nome}, Altura: {altura} mt")
+# Mostra os dados
+print(f"Usuário: {nome}, Altura: {altura} m")
 
+# Entrada de notas
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
 
-## Nova Feature ##
+# Cálculo da soma e média
+soma_notas = nota1 + nota2
+media_notas = soma_notas / 2
 
-primeiraNota = float(input("Digite sua primeira nota: "))
-segundaNota = float(input("Digite sua segunda nota: "))
-soma = primeiraNota + segundaNota
-mediaNotas = soma / 2
-
-print(f"A soma das notas é: {soma:.2f} e sua média foi de {mediaNotas:.2f}")
-
-
+# Mostra resultados
+print(f"Soma das notas: {soma_notas:.2f}")
+print(f"Média das notas: {media_notas:.2f}")
